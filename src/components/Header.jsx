14f5ba08f2ae2c38224eb684660e1d008e0b5624
Gautam Sarkar <gautam_32b7@hotmail.com>
2025-08-02 08:49:13 +0530

    Add bottom border to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Moon } from "lucide-react";
 
 export default function Header() {
   return (
-    <header className="px-12 py-6 flex items-center justify-between shadow mb-6">
+    <header className="px-12 py-6 flex items-center justify-between border-b-1 border-gray-300 mb-6">
       <h1 className="text-2xl text-gray-950 font-bold">Where in the world?</h1>
       <button className="p-2 text-gray-950 flex items-center gap-1 font-medium cursor-pointer">
         <Moon />
