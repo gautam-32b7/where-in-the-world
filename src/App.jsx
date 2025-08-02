e7b5ba754961517c914a7c663a0536e6842004db
Gautam Sarkar <gautam_32b7@hotmail.com>
2025-08-02 16:27:42 +0530

    Add CountriesGrid component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 import Header from "./components/Header.jsx";
 import SearchFilterByRegionFlex from "./components/SearchFilterByRegionFlex.jsx";
+import CountriesGrid from "./components/CountriesGrid.jsx";
+
+import { data } from "./assets/data.js";
 
 export default function App() {
+  const [countries, setCountries] = useState(data);
+
   return (
     <div className="max-w-7xl mx-auto">
       <Header />
       <SearchFilterByRegionFlex />
+      <CountriesGrid />
     </div>
   );
 }
---
+++ src/components/CountriesGrid.jsx
@@ -0,0 +1,3 @@
+export default function CountriesGrid({ countries }) {
+  return <div className="px-12 py-6">COUNTRIES GRID</div>;
+}
