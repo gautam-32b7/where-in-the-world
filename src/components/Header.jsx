cd83097baebdc349bb6185305dfa2ed845819a22
Gautam Sarkar <gautam_32b7@hotmail.com>
2025-08-13 10:10:42 +0530

    Remove dark mode button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,6 @@ export default function Header() {
   return (
     <header className="px-12 py-6 flex items-center justify-between border-b-1 border-gray-300 mb-6">
       <h1 className="text-2xl text-gray-950 font-bold">Where in the world?</h1>
-      <button className="p-2 text-gray-950 flex items-center gap-1 font-medium cursor-pointer">
-        <Moon />
-        <span>Dark Mode</span>
-      </button>
     </header>
   );
 }
