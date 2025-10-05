1c9571780359527aed1cbb64af6fbfb874cd9258
Gautam Sarkar <gautam_32b7@hotmail.com>
2025-10-05 09:33:56 +0530

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router";
 
+import Header from "./components/Header";
+
 export default function App() {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto">
+      <Header />
       <Outlet />
     </div>
   );
