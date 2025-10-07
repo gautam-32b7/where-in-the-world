64e08d37e1dd479f2e8953a5a9f3a5b98f1e945a
gautam <gautam_32b7@hotmail.com>
2025-10-07 18:03:07 +0530

    add Not Found page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <div className="min-h-screen">
+      <h2 className="text-3xl text-gray-900 font-medium text-center p-3">
+        OOPS!
+      </h2>
+      <p className="text-gray-700 text-center p-3">
+        The Page You Requested Could Not Be Found.
+      </p>
+      <h2 className="text-3xl text-gray-900 font-medium text-center p-3">👻</h2>
+    </div>
+  );
+}
